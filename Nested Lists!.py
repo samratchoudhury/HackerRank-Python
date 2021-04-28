f6b07b314678dbcc3ee0132e42ce8d9445bc17de
samratchoudhury <73993794+samratchoudhury@users.noreply.github.com>
2021-04-28 23:50:23 +0530

    Create Nested Lists!.py

---
+++ Nested Lists!.py
@@ -0,0 +1,67 @@
+# it is in Python 3 version.
+from __future__ import print_function
+score_list = {}
+for _ in range(input()):
+    name = raw_input()
+    score = float(raw_input())
+    if score in score_list:
+        score_list[score].append(name)
+    else:
+        score_list[score] = [name]
+new_list = []
+for i in score_list:
+    new_list.append([i, score_list[i]])
+new_list.sort()
+result = new_list[1][1]
+result.sort()
+print (*result, sep = "\n")
+'''
+Given the names and grades for each student in a class of  students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+
+Note: If there are multiple students with the second lowest grade, order their names alphabetically and print each name on a new line.
+
+Example
+
+The ordered list of scores is , so the second lowest score is . There are two students with that score: . Ordered alphabetically, the names are printed as:
+
+alpha
+beta
+Input Format
+
+The first line contains an integer, , the number of students.
+The  subsequent lines describe each student over  lines.
+- The first line contains a student's name.
+- The second line contains their grade.
+
+Constraints
+
+There will always be one or more students having the second lowest grade.
+Output Format
+
+Print the name(s) of any student(s) having the second lowest grade in. If there are multiple students, order their names alphabetically and print each one on a new line.
+
+Sample Input 0
+
+5
+Harry
+37.21
+Berry
+37.21
+Tina
+37.2
+Akriti
+41
+Harsh
+39
+Sample Output 0
+
+Berry
+Harry
+Explanation 0
+
+There are  students in this class whose names and grades are assembled to build the following list:
+
+python students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+
+The lowest grade of  belongs to Tina. The second lowest grade of  belongs to both Harry and Berry, so we order their names alphabetically and print each name on a new line.
+'''
